d6476ec885a71fecbef29c301f0dfe959122e7c8
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2024-05-15 02:03:05 +0530

    New

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -95,7 +95,7 @@ const Header = ({ user, activeItem,isSellerExist }: Props) => {
             <h1>
               <Link href={"/"}>
                 <h1 className="font-Inter text-3xl cursor-pointer">
-                  <span className="text-[#64ff4c]">Bec</span>odemy
+                  <span className="text-[#64ff4c]">Market</span>place
                 </h1>
               </Link>
             </h1>
